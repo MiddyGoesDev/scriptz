20103dc99fd836bb0ba2eba8014a22c0dc578d83
Finn Rietz <19836078+frietz58@users.noreply.github.com>
2020-03-17 19:06:09 +0100

    Add script that gives user ownership and rwx permissions for given dir

---
+++ bash/chownmod.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------------
+# |	Run with sudo, we need sudo for chmod and chown...			     |
+# |										     |	
+# |	Sets given user as owner for a directory and all items in that directory     |
+# |	alongside giving that user read, write and execute permissions for           |
+# |	that directory. Script can also be use to take right away for a user, but    |
+# |     primary usecase is to conveniently give all rights, hence the default args.  |
+# |										     |	
+# |	Example usage: 								     |
+# |     $ ./chownmod /path/to/target_dir				       	     |	
+# |	This binds the only given (unnamed) argument to the $TARGET_DIR variable     |	
+# |	and uses the default values for the other args.				     |	
+# |										     |	
+# |	$ ./chownmod user="username" access="a" op=+" perms="rwx" dir="dir/path"     |	
+# |	Here we provide all arguments directly via CLI.				     |	
+# |										     |	
+# |										     |	
+# |	@author: Finn Rietz							     |	
+# -------------------------------------------------------------------------------------
+
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            user)              USER=${VALUE} ;;
+            access)            ACCESS=${VALUE} ;;     
+	    perms)             PERMS=${VALUE} ;;
+	    op)                OP=${VALUE} ;;
+	    dir) 	       TARGET_DIR=${VALUE} ;;
+            *)  	       TARGET_DIR=$ARGUMENT ;; 
+    esac    
+
+done
+
+# check whether all variables are set
+# $USER is set by default on linux systems
+if [ -z "$USER" ]
+then 
+	echo "Argument 'user' can not be empty."
+	echo "Please provide like: ./chownmod.sh user=\"username\""
+	exit 2
+fi
+
+# the directory to which we want to apply chown and chmod
+if [ -z "$TARGET_DIR" ]
+then 
+	echo "Argument 'dir' can not be empty."
+	echo "Please provide liek: ./chownmod.sh dir=\"target/dir\""
+	exit 2
+fi
+
+# which access to change with chmod
+if [ -z "$ACCESS" ]
+then 
+	ACCESS="a"
+fi
+
+# which permissions to apply with chmod
+if [ -z "$PERMS" ]
+then 
+	PERMS="rwx"
+fi
+
+# which operator to apply with chmod (give perms, take perms etc)
+if [ -z "$OP" ]
+then 
+	OP="+"
+fi
+
+echo ""
+echo "Got the following argument values (may be partially set default):"
+echo "USER: $USER"
+echo "ACCESS: $ACCESS"
+echo "OP: $OP"
+echo "PERMS: $PERMS"
+echo "TARGET_DIR: $TARGET_DIR"
+echo ""
+
+echo "executing resulting calls:"
+echo "sudo chown -R $USER $DIR"
+echo "sudo chmod -R $ACCESS$OP$PERMS $TARGET_DIR"
+echo ""
+
+sudo chown -R $USER $TARGET_DIR
+sudo chmod -R $ACCESS$OP$PERMS $TARGET_DIR
+
+echo "Done"
+echo ""
+exit 0
+
